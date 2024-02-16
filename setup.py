f3ffb66707a778f42a5df4348b84f1d528247d61
Santosh Philip <santosh@noemail.com>
2024-02-16 09:42:17 -0800

    rolled back to version 0.1.0

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/santoshphilip/pysunnoaa",
-    version="version='0.1.2'",
+    version="0.1.0",
     zip_safe=False,
 )
